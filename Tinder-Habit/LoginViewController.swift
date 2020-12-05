240aa718257b1dac3af5dc131d401070e865b288
Trang Doan <doantranghp2000@gmail.com>
2020-12-05 02:09:06 +0100

    ver that works

--- Tinder-Habit/LoginViewController.swift
+++ Tinder-Habit/LoginViewController.swift
@@ -12,51 +12,33 @@ import GoogleSignIn
 
 
 class LoginViewController: UIViewController, GIDSignInDelegate {
-    
-    
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        if let authentication = user.authentication {
-            let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
-
-            Auth.auth().signIn(with: credential, completion: { (user, error) -> Void in
-                if error != nil {
-                    print("Problem at signing in with google with error : \(String(describing: error))")
-                }
-                if user != nil {
-                    self.performSegue(withIdentifier: "logInToHome", sender: self)
-                    print("user successfully signed in through GOOGLE! user_id:\(Auth.auth().currentUser!.uid)")
-                    print("signed in")
-                }
-            })
-            
+        if let error = error {
+        print(error.localizedDescription)
+        return
+        }
+        guard let auth = user.authentication else { return }
+        let credentials = GoogleAuthProvider.credential(withIDToken: auth.idToken, accessToken: auth.accessToken)
+        Auth.auth().signIn(with: credentials) { (authResult, error) in
+        if let error = error {
+        print(error.localizedDescription)
+        } else {
+            print("Login successful")
+            self.performSegue(withIdentifier: "logInToHome", sender: self)
         }
     }
-
-        
+    }
+         
     @IBAction func googleBtnTapped(_ sender: Any) {
         GIDSignIn.sharedInstance().signIn()
     }
-    // Get credential object using Google ID token and Google access token
-//        guard let authentication = user.authentication else { return }
-//
-//        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
-//                                                        accessToken: authentication.accessToken)
-//
-//        // Authenticate with Firebase using the credential object
-//        Auth.auth().signIn(with: credential) { (authResult, error) in
-//            if let error = error {
-//                print("authentication error \(error.localizedDescription)")
-//                }
-//            }
-//        }
-    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         GIDSignIn.sharedInstance()?.presentingViewController = self
-        GIDSignIn.sharedInstance().signIn()
+//        GIDSignIn.sharedInstance().signIn()
         GIDSignIn.sharedInstance().delegate = self
         // Do any additional setup after loading the view.
         
@@ -64,3 +46,4 @@ class LoginViewController: UIViewController, GIDSignInDelegate {
 
 }
 }
+
