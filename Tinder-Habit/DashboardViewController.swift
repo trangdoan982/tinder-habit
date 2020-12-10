a8635361a73a78b194cf41b258d5ca0ecaae3e7e
Brian Keane <lonesomewhistle@gmail.com>
2020-12-10 13:48:09 -0600

    viewController changes cut and pasted in

---
+++ Tinder-Habit/DashboardViewController.swift
@@ -0,0 +1,95 @@
+//
+//  DashboardViewController.swift
+//  Tinder-Habit
+//
+//  Created by Brian Keane on 12/10/20.
+//  Copyright © 2020 Trang Doan. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    updateUI()
+    view.addSubview(lineChartView)
+    lineChartView.center(in: view, offset: CGPoint(x: 0, y: 63))
+    
+    lineChartView.width(350)
+    lineChartView.height(250)
+    
+    setData()
+  }
+  
+  let userInfo:[String:String] = [
+    "username": "Hello, Robert",
+    "profileImageURL": "https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/profile_user.jpg",
+    "biotext" : "Robert wants to workout 4-5 days a week"
+    
+  ]
+  
+  
+  lazy var lineChartView: LineChartView = {
+    let chartView = LineChartView()
+    chartView.backgroundColor = UIColor(displayP3Red: 1.0, green: 0.8, blue: 0.9, alpha: 0.5 )
+    
+    chartView.rightAxis.enabled = false
+    
+    let yAxis = chartView.leftAxis
+    yAxis.labelFont = .boldSystemFont(ofSize: 12)
+    yAxis.setLabelCount(3, force: false)
+    yAxis.labelTextColor = .black
+    yAxis.axisLineColor = .black
+    
+    
+    chartView.xAxis.labelPosition = .bottom
+    chartView.xAxis.labelFont = .boldSystemFont(ofSize: 12)
+    chartView.xAxis.setLabelCount(6, force: false)
+    chartView.xAxis.labelTextColor = .black
+    
+    chartView.animate(xAxisDuration: 4.0)
+    return chartView
+  }()
+  
+  func updateUI(){
+    profileImage.kf.setImage(with: URL(string: self.userInfo["profileImageURL"]!))
+    self.userName.text = userInfo["username"]
+    self.bioText.text = userInfo["biotext"]
+  }
+  
+  
+  func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+    print(entry)
+    
+  }
+  
+  func setData(){
+    let set1 = LineChartDataSet(entries: yValues, label: "Your Progress")
+    
+    // set1.mode = .cubicBezier
+    set1.lineWidth = 2
+    set1.setColor(.black)
+    set1.setCircleColor(.black)
+    set1.circleRadius = 5
+    set1.circleHoleRadius = 2
+    set1.circleHoleColor = .white
+    
+    let data = LineChartData(dataSet: set1)
+    lineChartView.data = data
+  }
+  
+  
+  let yValues: [ChartDataEntry] = [
+    ChartDataEntry(x: 0.0, y: 1.0),
+    ChartDataEntry(x: 1.0, y: 5.0),
+    ChartDataEntry(x: 2.0, y: 10.0),
+    ChartDataEntry(x: 4.0, y: 12.0),
+    ChartDataEntry(x: 6.0, y: 15.0),
+    ChartDataEntry(x: 8.3, y: 17.0),
+    ChartDataEntry(x: 10.2, y: 12.0),
+    ChartDataEntry(x: 15.2, y: 20.3),
+    ChartDataEntry(x: 20.0, y: 23.0),
+    ChartDataEntry(x: 25.0, y: 25.0),
+    ChartDataEntry(x: 29.0, y: 30.1)
+  ]
+}
---
+++ Tinder-Habit/MatchingViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MatchingViewController.swift
+//  Tinder-Habit
+//
+//  Created by Brian Keane on 12/10/20.
+//  Copyright © 2020 Trang Doan. All rights reserved.
+//
+
+import UIKit
+
+class MatchingViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  var profileImageURLStrings: [String] = []
+  var profileBio: [String] = []
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    profileImageURLStrings = [
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "https://images.pexels.com/photos/2078265/pexels-photo-2078265.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "https://images.pexels.com/photos/1310522/pexels-photo-1310522.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    ]
+    
+    kolodaView.dataSource = self
+    kolodaView.delegate = self
+    
+  }
+  
+  func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
+    // kolodaView.isHidden = true
+    print("ran out of cards")
+  }
+  
+  func koloda(_ koloda: KolodaView, didSelectCardAt index: Int) {
+    print("selected \(index)")
+  }
+  
+  func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+    return profileImageURLStrings.count
+  }
+  
+  func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
+    return .fast
+  }
+  
+  func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+    let imageView = UIImageView()
+    let url = URL(string: profileImageURLStrings[index])
+    imageView.kf.setImage(with: url)
+    return imageView
+  }
+}
